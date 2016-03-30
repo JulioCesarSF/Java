777823a4c61f37cd9b8162c94f1f09affd0f481c
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-03-30 13:41:38 -0300

    Create HelloWorld.java

---
+++ HelloWorld.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		/*
+		 * Para imprimir texto no console: System.out.println (pula linha) e só print não pula linha.
+		 * 
+		 * */
+		
+		System.out.println("Hello World!");
+
+	}
+
+}
