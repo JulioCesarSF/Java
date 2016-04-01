b6704fbc115446ce151a9d18072f61e86cbb35ea
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-04-01 20:36:51 -0300

    Create Contato.java

---
+++ 1berto/Getters-Setters-Exercicios/Ebook/Contato.java
@@ -0,0 +1,23 @@
+package descansos.Ebook;
+
+public class Contato {
+	
+	public static String nome, telefone;	
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		Contato.nome = nome;
+	}
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+	public void setTelefone(String telefone) {
+		Contato.telefone = telefone;
+	}	
+
+}
