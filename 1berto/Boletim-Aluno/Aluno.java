6ae919acb540d10d1b8cf8ad0d6a20ff367b0a75
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-04-01 18:38:25 -0300

    Update Aluno.java

--- 1berto/Boletim-Aluno/Aluno.java
+++ 1berto/Boletim-Aluno/Aluno.java
@@ -7,6 +7,16 @@ public class Aluno {
 	private String nome, fone;
 	private int rm;
 	
+	/* *** Construtores *** */
+	
+	public Aluno(){}
+	
+	public Aluno(String nome, String fone, int rm){
+		this.nome = nome;
+		this.fone = fone;
+		this.rm = rm;
+	}
+	
 	/* *** Métodos gerados pelo Eclipse *** 
 	 * Menu: "Source" -> "Generate Getters and Setters..." -> "Select All" -> "OK"
 	 * 
