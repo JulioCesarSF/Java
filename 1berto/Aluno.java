4229c8fbb38a96a868f0d483c45baf17f965c9e7
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-04-01 18:09:08 -0300

    Create Aluno.java

---
+++ 1berto/Aluno.java
@@ -0,0 +1,36 @@
+package br.com.fiap.modelo;
+
+public class Aluno {
+	
+	/* *** Atributos *** */
+	
+	private String nome, fone;
+	private int rm;
+	
+	/* *** Métodos gerados pelo Eclipse *** 
+	 * Menu: "Source" -> "Generate Getters and Setters..." -> "Select All" -> "OK"
+	 * 
+	 * */
+	
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getFone() {
+		return fone;
+	}
+	public void setFone(String fone) {
+		this.fone = fone;
+	}
+	public int getRm() {
+		return rm;
+	}
+	public void setRm(int rm) {
+		this.rm = rm;
+	}
+	
+	
+
+}
