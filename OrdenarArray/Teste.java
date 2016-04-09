59d55ade67dd4ec6911648e04750c6d3fa4d423e
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-04-09 10:19:35 -0300

    Update Teste.java

--- OrdenarArray/Teste.java
+++ OrdenarArray/Teste.java
@@ -6,28 +6,34 @@ import modelos.ListaAluno;
 public class Teste {
 
 	public static void main(String[] args) {
-		ListaAluno lista = new ListaAluno();
 		
-		lista.addAluno(lista.getListaAlunos(), new Aluno("G"));
-		lista.addAluno(lista.getListaAlunos(), new Aluno("F"));
-		lista.addAluno(lista.getListaAlunos(), new Aluno("A"));
-		
-		System.out.println("Teste 1 não ordenada:");
-		lista.listar();		
-		
-		System.out.println("Teste 1 ordenada:");
-		lista.ordenar();		
-		lista.listar();		
-		
-		System.out.println("Teste 2 não ordenada:");
-		lista.addAluno(new Aluno("Z"));
-		lista.addAluno(new Aluno("W"));
-		lista.addAluno(new Aluno("B"));		
-		lista.listar();
-		
-		System.out.println("Teste 2 ordenada:");		
-		lista.ordenar();		
-		lista.listar();
+		try{
+			ListaAluno lista = new ListaAluno();
+			
+			lista.addAluno(lista.getListaAlunos(), new Aluno("G"));
+			lista.addAluno(lista.getListaAlunos(), new Aluno("F"));
+			lista.addAluno(lista.getListaAlunos(), new Aluno("A"));
+			
+			System.out.println("Teste 1 não ordenada:");
+			lista.listar();		
+			
+			System.out.println("Teste 1 ordenada:");
+			lista.ordenar();		
+			lista.listar();		
+			
+			System.out.println("Teste 2 não ordenada:");
+			lista.addAluno(new Aluno("Z"));
+			lista.addAluno(new Aluno("W"));
+			lista.addAluno(new Aluno("B"));		
+			lista.listar();
+			
+			System.out.println("Teste 2 ordenada:");		
+			lista.ordenar();		
+			lista.listar();			
+		}
+		catch(Exception e){
+			System.out.println("Ocorreu um problema: " + e.getMessage());
+		}
 
 	}
 
