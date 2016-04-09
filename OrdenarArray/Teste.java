a921a7b28fcf8150e6cbfb06eb57f68dd5c33f9b
Júlio César Schincariol Filho <julio.com@uol.com.br>
2016-04-09 12:08:13 -0300

    Update Teste.java

--- OrdenarArray/Teste.java
+++ OrdenarArray/Teste.java
@@ -8,28 +8,14 @@ public class Teste {
 	public static void main(String[] args) {
 		
 		try{
-			ListaAluno lista = new ListaAluno();
+			ListaAluno lista = new ListaAluno();						
 			
-			lista.addAluno(lista.getListaAlunos(), new Aluno("G"));
-			lista.addAluno(lista.getListaAlunos(), new Aluno("F"));
-			lista.addAluno(lista.getListaAlunos(), new Aluno("A"));
+			lista.addAluno(new Aluno("Chardcen"));
+			lista.addAluno(new Aluno("Wynsung"));
+			lista.addAluno(new Aluno("Adelwald"));
+			lista.addAluno(new Aluno("Isenfast"), true);				
 			
-			System.out.println("Teste 1 não ordenada:");
-			lista.listar();		
-			
-			System.out.println("Teste 1 ordenada:");
-			lista.ordenar();		
-			lista.listar();		
-			
-			System.out.println("Teste 2 não ordenada:");
-			lista.addAluno(new Aluno("Z"));
-			lista.addAluno(new Aluno("W"));
-			lista.addAluno(new Aluno("B"));		
-			lista.listar();
-			
-			System.out.println("Teste 2 ordenada:");		
-			lista.ordenar();		
-			lista.listar();			
+			lista.listar();				
 		}
 		catch(Exception e){
 			System.out.println("Ocorreu um problema: " + e);
